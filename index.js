b82c503de72b3df1d427d13ae3f321e123332b4c
weibor <yuweibor@me.com>
2022-06-08 02:09:59 +0800

    1

--- index.js
+++ index.js
@@ -3,57 +3,80 @@ const fs = require('fs');
 const http = require('http');
 const https = require('https');
 const cheerio = require('cheerio');
-
-const makeFileName = () => {
-  return `${ new Date().toLocaleString() }`
+const moment = require('moment');
+const getDayRange = days => {
+  const dayRange = [];
+  for (let i = 0; i < days; i++) {
+    const newDay = moment().subtract(i, 'days').format('YYYY-MM-DD');
+    dayRange.push(newDay);
+  }
+  return dayRange;
 }
 
-const run = async (address, key, level) => {
-  let hrefArr = []
-  let arr = []
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-  const ru = () => {
-    return new Promise((res, rej) => {
-      const runner = (_address, _level) => {
-        if (_level === -1) res(arr);
-        const method = _address.startsWith('http://') > -1 ? http : https;
+const run = (address, keys, level, fileName, days) => {
+  if (fileName) fs.rm(fileName, () => { })
+  fs.rm("error.txt", () => { })
+  const dayRange = getDayRange(days);
+  let hrefArr = [];
+  let count = 0;
+  let _level = level;
+  let buffer = 50;
 
-        method.get(_address, function (res) {
-
-          // 分段返回的 自己拼接
-          let html = '';
-          // 有数据产生的时候 拼接
-          res.on('data', function (chunk) {
-            html += chunk;
-          })
-          // 拼接完成
-          res.on('end', function () {
-            const $ = cheerio.load(html);
-            $('a').each(function () {
-              const href = $(this).attr("href");
-
-              if (typeof href === 'string' && hrefArr.indexOf(href) === -1 && href.startsWith("http")) {
-                arr.push({
-                  text: $(this).text(),
-                  href,
-                  hasKey: html.indexOf(key) > -1
-                })
-                hrefArr.push(href);
-                hrefArr = Array.from(new Set(hrefArr));
-                runner(href, _level--)
-              }
-
-            })
-          })
+  const runner = async (_address, originUrl) => {
+    while (buffer < 0) {
+      await new Promise(e => setTimeout(e, 1000))
+    }
+    buffer--;
+    if (_level <= 0) return;
+    if (_address.indexOf('/') < 0) return;
+    let url = new URL(_address, originUrl);
+    if (!url) return;
+    console.log(`${ ++count }次点击,${ level - _level }层`)
+    const method = url.protocol.indexOf('s') > -1 ? https : http;
+    method.get(url.href, (res) => {
+      console.log(res.statusCode, buffer);
+      let html = "";
+      res.on("data", (chunk) => {
+        html += chunk;
+      })
+      res.on("end", () => {
+        buffer++;
+        _level--;
+        const inDayRange = dayRange.filter(s => {
+          return html.indexOf(s) > -1
         })
-      }
-      runner(address, level);
+        const hasKey = keys.filter(s => {
+          return html.indexOf(s) > -1
+        })
+        if (hasKey.length && inDayRange.length) {
+          fs.appendFileSync(fileName,
+            `-----网址:${ url.origin }${ _address }-----\n
+               --关键词:${ hasKey.join("、") }--\n
+               --日期:${ inDayRange.join("、") }--\n
+               --第${ _level }层检索\n
+            `)
+        }
+        const $ = cheerio.load(html);
+        $("a").each(function () {
+          const href = $(this).attr("href");
+          if (typeof href === "string" && hrefArr.indexOf(href + url.origin) === -1) {
+            hrefArr.push(href + url.origin);
+            hrefArr = Array.from(new Set(hrefArr));
+            runner(href, url.origin);
+          }
+
+        })
+      })
+    }).on("error", e => {
+      buffer++;
+      console.log('err', e.message, e.name)
+      fs.appendFileSync("error.txt", `error--${ url.href }  ${ url.origin }\n${ e.message }\n`)
+      return;
     })
   }
-  const addrArr = await ru();
-  fs.writeFile(`${ makeFileName() }.txt`, JSON.stringify(addrArr), () => { })
+  runner(address);
 }
-
-
-run("https://movie.douban.com/top250", "水泥", 1)
-// await runner(address, level);
+//网址 /关键词/深度/文件名/最近多少天
+run("http://www.gov.cn", [ "水泥", "错峰生产", "环保督察", "产能过剩", "转型升级", "技改", "绿色建材产业园", "绿色工厂", "智能制造" ], 20000, "a.txt", 7)
