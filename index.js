4e1f8c750e2e275648179ab4a4589234e16a7e4c
weibor <yuweibor@me.com>
2022-06-23 02:41:12 +0800

    换axios

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 const fs = require('fs');
-const http = require('http');
-const https = require('https');
+const { get } = require("axios");
 const cheerio = require('cheerio');
 const moment = require('moment');
 const getDayRange = days => {
@@ -13,72 +12,59 @@ const getDayRange = days => {
   return dayRange;
 }
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
-const run = (address, keys, level, fileName, days) => {
+const run = (address, keys, fileName, days) => {
+  //清理旧文件
   if (fileName) fs.rm(fileName, () => { })
   fs.rm("error.txt", () => { })
   const dayRange = getDayRange(days);
   let hrefArr = [];
+  let lock = false;
   let count = 0;
-  let _level = level;
-  let buffer = 5;
-
   const runner = async (_address, originUrl) => {
-    while (buffer < 0) {
-      await new Promise(e => setTimeout(e, 1000))
-    }
-    buffer--;
-    if (_level <= 0) return;
-    _level--;
+    _address = _address.trim();
     if (_address.indexOf('/') < 0) return;
     let url = new URL(_address, originUrl);
     if (!url) return;
-    console.log(`${ ++count }次点击,${ level - _level }层`)
-    const method = url.protocol.indexOf('s') > -1 ? https : http;
-    method.get(url.href, (res) => {
-      console.log(res.statusCode, buffer);
-      let html = "";
-      res.on("data", (chunk) => {
-        html += chunk;
+    if (lock) {
+      await new Promise(e => setTimeout(e, 50));
+      runner(_address, originUrl);
+      return;
+    }
+    lock = true;
+    get(url.href).then((res) => {
+      count++;
+      let html = res.data;
+      const inDayRange = dayRange.filter(s => {
+        return html.indexOf(s) > -1
       })
-      res.on("end", () => {
-
-        const inDayRange = dayRange.filter(s => {
-          return html.indexOf(s) > -1
-        })
-        const hasKey = keys.filter(s => {
-          return html.indexOf(s) > -1
-        })
-        if (hasKey.length && inDayRange.length) {
-          fs.appendFileSync(fileName,
-            `-----网址:${ url.origin }${ _address }\n
+      const hasKey = keys.filter(s => {
+        return html.indexOf(s) > -1
+      })
+      if (hasKey.length && inDayRange.length) {
+        fs.appendFileSync(fileName,
+          `-----网址: ${ _address.indexOf("://") > -1 ? _address : url.origin + _address }\n
                --关键词:${ hasKey.join("、") }--\n
                --日期:${ inDayRange.join("、") }--\n
-               --第${ _level }层检索\n
-            `)
+          `)
+      }
+      const $ = cheerio.load(html);
+      $("a").each(function () {
+        const href = $(this).attr("href");
+        if (typeof href === "string" && hrefArr.indexOf(href + url.origin) === -1) {
+          hrefArr.push(href + url.origin);
+          hrefArr = Array.from(new Set(hrefArr));
+          runner(href, url.origin);
         }
-        const $ = cheerio.load(html);
-        $("a").each(function () {
-          const href = $(this).attr("href");
-          if (typeof href === "string" && hrefArr.indexOf(href + url.origin) === -1) {
-            hrefArr.push(href + url.origin);
-            hrefArr = Array.from(new Set(hrefArr));
-            runner(href, url.origin);
-          }
 
-        })
       })
-    }).on("error", e => {
-      buffer++;
+    }).catch(e => {
+      lock = false;
       console.log('err', e.message, e.name)
-      fs.appendFileSync("error.txt", `error--${ url.href }  ${ url.origin }\n${ e.message }\n`)
+      fs.appendFileSync("error.txt", `error--${ url.href }  ${ url.origin }\n${ e.message }\n ${ url.protocol }\n`)
       return;
-    }).on("finish", () => {
-      buffer++;
-    })
+    }).finally(() => { lock = false; console.log(count) })
   }
   runner(address);
 }
-//网址 /关键词/深度/文件名/最近多少天
-run("http://www.gov.cn", [ "水泥", "错峰生产", "环保督察", "产能过剩", "转型升级", "技改", "绿色建材产业园", "绿色工厂", "智能制造" ], 20000, "a.txt", 7)
+//网址 /关键词/文件名/最近多少天
+run("http://www.gov.cn", [ "水泥", "错峰生产", "环保督察", "产能过剩", "转型升级", "技改", "绿色建材产业园", "绿色工厂", "智能制造" ], "a.txt", 7)
